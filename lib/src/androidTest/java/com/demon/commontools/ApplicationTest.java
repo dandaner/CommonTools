4f65ecf0df4fe9cb042cf659277e2c88023a64b7
demon.zhanglei <demon.zhanglei@dianxinos.com>
2015-03-26 17:14:39 +0800

    [master] change:给NetworkUtil添加测试用例，测试通过，并记录一些理解在lib/src/androidTest/AndroidManifest.xml中

--- lib/src/androidTest/java/com/demon/commontools/ApplicationTest.java
+++ lib/src/androidTest/java/com/demon/commontools/ApplicationTest.java
@@ -1,7 +1,11 @@
 package com.demon.commontools;
 
 import android.app.Application;
+import android.content.Context;
 import android.test.ApplicationTestCase;
+
+import com.demon.commontools.net.NetworkUtil;
+import com.demon.commontools.utils.LogUtils;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +13,17 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    private static final String TAG = "ApplicationTest";
+
+    public void testNetworkUtil() {
+        Context ctx = getContext();
+        boolean enable = NetworkUtil.isNetworkAvaialble(ctx);
+        int networkType = NetworkUtil.getNetworkType(ctx);
+        int simpleNetworkType = NetworkUtil.getSimpleNetworkType(ctx);
+        LogUtils.d(TAG, "enable = " + enable + "  networkType = "
+                + networkType + "   simpleNetworkType = " + simpleNetworkType);
+//        throw new RuntimeException("ahahahha");
     }
 }
